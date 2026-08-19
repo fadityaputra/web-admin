b8adb27e9a65fc78ba44733a0b61318a0b49efb7
Aditya Putra <backsidetheblackwings@gmail.com>
2026-08-19 16:59:19 +0700

    force-dynamic

--- app/api/[storeId]/banners/route.ts
+++ app/api/[storeId]/banners/route.ts
@@ -1,82 +1,81 @@
-import { NextResponse } from "next/server";
-import { auth } from "@clerk/nextjs/server";
-import db from "@/lib/db";
+export const dynamic = 'force-dynamic'
 
+import { NextResponse } from 'next/server'
+import { auth } from '@clerk/nextjs/server'
+import db from '@/lib/db'
 
-export async function POST(req: Request,
-    {params}: {params: {storeId: string}}
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
 ) {
-    try {
-        const { userId } = await auth();
-        console.log("UserID =>", userId);
+  try {
+    const { userId } = await auth()
+    console.log('UserID =>', userId)
 
-        const body = await req.json();
+    const body = await req.json()
 
-        const {label, imageUrl} = body;
+    const { label, imageUrl } = body
 
-        if (!userId) {
-            return new NextResponse("Unauthorized", { status: 401 });
-          }
-
-        if (!label) {
-            return new NextResponse("nama banner perlu di input", { status: 400 });
-          }
-
-          if (!imageUrl) {
-            return new NextResponse("image banner perlu di input", { status: 400 });
-          }
-
-          if (!params.storeId) {
-            return new NextResponse("store id url dibutuhkan")
-          }
-
-          const storeByUserId = await db.store.findFirst({
-            where: {
-                id: params.storeId,
-                userId
-            }
-          })
-
-          if (!storeByUserId) {
-            return new NextResponse("Unauthorized", {status: 403})
-          }
-
-        const banner = await db.banner.create({
-            data:{
-                label,
-                imageUrl,
-                storeId: params.storeId
-            }
-        })
-
-        return NextResponse.json(banner);
-        
-    } catch (error) {
-        console.log("[BANNERS_POST]", error)
-        return new NextResponse("Internal Server Error", { status: 500 });
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 })
     }
+
+    if (!label) {
+      return new NextResponse('nama banner perlu di input', { status: 400 })
+    }
+
+    if (!imageUrl) {
+      return new NextResponse('image banner perlu di input', { status: 400 })
+    }
+
+    if (!params.storeId) {
+      return new NextResponse('store id url dibutuhkan')
+    }
+
+    const storeByUserId = await db.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    })
+
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized', { status: 403 })
+    }
+
+    const banner = await db.banner.create({
+      data: {
+        label,
+        imageUrl,
+        storeId: params.storeId,
+      },
+    })
+
+    return NextResponse.json(banner)
+  } catch (error) {
+    console.log('[BANNERS_POST]', error)
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
 }
 
-export async function GET(req: Request,
-    {params}: {params: {storeId: string}}
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
 ) {
-    try {
-
-          if (!params.storeId) {
-            return new NextResponse("store id url dibutuhkan")
-          }
-
-
-        const banner = await db.banner.findMany({
-            where: {
-              storeId: params.storeId
-            }
-        })
-
-        return NextResponse.json(banner);
-        
-    } catch (error) {
-        console.log("[BANNERS_GET]", error)
-        return new NextResponse("Internal Server Error", { status: 500 });
+  try {
+    if (!params.storeId) {
+      return new NextResponse('store id url dibutuhkan')
     }
+
+    const banner = await db.banner.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    })
+
+    return NextResponse.json(banner)
+  } catch (error) {
+    console.log('[BANNERS_GET]', error)
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
 }
