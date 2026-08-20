8c8444844d8806a7a657d3e98eeba1a0a441d2b6
Aditya Putra <backsidetheblackwings@gmail.com>
2026-08-20 08:59:03 +0700

    force-dynamic

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,30 +1,28 @@
-import db from "@/lib/db";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
+import db from '@/lib/db'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+export const dynamic = 'force-dynamic'
 
 export default async function SetupLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    const { userId } = await auth();
-    if (!userId) {
-      redirect("/sign-in"); 
-    }
-  
-    const store = await db.store.findFirst({
-      where: {
-        userId: userId,
-      },
-    });
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const { userId } = await auth()
+  if (!userId) {
+    redirect('/sign-in')
+  }
 
-    if (store) {
-        redirect(`/${store.id}`); 
-      }
-    
-      return (
-        <>
-          {children}
-        </>
-      );
-    }
+  const store = await db.store.findFirst({
+    where: {
+      userId: userId,
+    },
+  })
+
+  if (store) {
+    redirect(`/${store.id}`)
+  }
+
+  return <>{children}</>
+}
